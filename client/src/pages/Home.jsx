2ae24dac12644f8725879471dab81f2377264495
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-31 23:19:14 +0300

    created component: Slider.jsx + styled components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { Announcement } from "../components/Announcement";
 import { Navbar } from "../components/Navbar";
+import { Slider } from "../components/Slider";
 
 export const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
     </div>
   );
 };
