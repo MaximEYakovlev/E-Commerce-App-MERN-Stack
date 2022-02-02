5fff850b315abb84aeb0f447befda37cedd0774c
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-02 11:47:12 +0300

    created component Login.jsx + styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 import { Product } from "./pages/Product";
 import { ProductList } from "./pages/ProductList";
 import { Signup } from "./pages/Signup";
 
 function App() {
-  return <Signup />;
+  return <Login />;
 }
 
 export default App;
