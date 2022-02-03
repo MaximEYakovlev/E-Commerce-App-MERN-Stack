c3571ff240b644205341249e669670792d913886
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-03 23:37:29 +0300

    created server, added .gitignore, .env files

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
+const express = require("express");
+require("dotenv").config();
 
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(5000, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
