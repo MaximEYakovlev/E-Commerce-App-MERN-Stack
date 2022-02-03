7160d96f0136d2dbd3cb9c3a3859ce04be65fc2e
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-04 00:14:41 +0300

    deleted: node_modules

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 
 const app = express();
-
 const PORT = process.env.PORT || 5001;
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
