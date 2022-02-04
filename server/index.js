7dab61c57f334d3867288adf6a3c0b44ea0143f2
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-04 11:27:11 +0300

    connected the database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const PORT = process.env.PORT || 5001;
