2079819d74ed628bae375ccfe3686cb32aff9118
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-05 04:28:00 +0300

    added: route auth.js with the functionality of creating a new user

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 
 mongoose
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 
+app.use("/auth", authRoute);
 app.use("/users", userRoute);
 
 app.listen(PORT, () => {
