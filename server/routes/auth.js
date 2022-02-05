191e4b117836261122b0d522dfba7ce376afb6f1
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-05 20:20:27 +0300

    route auth.js: added a request handler router.post('/login'...)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -19,4 +19,22 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials!");
+    const decryptedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const originalPassword = decryptedPassword.toString(CryptoJS.enc.Utf8);
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!");
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
