f6e481748c4b7fbf7f79114c903f92e1433355eb
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-05 19:43:02 +0300

    used The Cipher Algorithms to encrypt the password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 router.post("/signup", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
