d3245012e14bc384ad47879ece5e94c6b93105b8
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-06 10:12:38 +0300

    route auth.js: added accessToken (JSON Web Token)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/signup", async (req, res) => {
   const newUser = new User({
@@ -30,8 +31,18 @@ router.post("/login", async (req, res) => {
     const originalPassword = decryptedPassword.toString(CryptoJS.enc.Utf8);
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong credentials!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
     const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
