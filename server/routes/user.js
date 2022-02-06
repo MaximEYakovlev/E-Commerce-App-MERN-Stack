2705682c7008bc65c4e5e0b41527ab32db5cd092
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-06 23:00:42 +0300

    routes folder / user.js file: added router.delete...

--- server/routes/user.js
+++ server/routes/user.js
@@ -23,4 +23,13 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
