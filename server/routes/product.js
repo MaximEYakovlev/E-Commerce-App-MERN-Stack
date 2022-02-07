9f6e69788a58326a4691e63052ebb40140c8ebdf
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-07 22:58:26 +0300

    routes folder / product.js file: added router.get (get products)

--- server/routes/product.js
+++ server/routes/product.js
@@ -45,4 +45,28 @@ router.get("/find/:id", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
