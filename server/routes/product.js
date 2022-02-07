1309b676b10f881239f391b72489fbbce5ebc87b
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-07 22:44:34 +0300

    routes folder / product.js file: added router.get (get specific Product)

--- server/routes/product.js
+++ server/routes/product.js
@@ -36,4 +36,13 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
