ebb201e07bad8f26491619595a62e67b6e933e16
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-08 07:13:42 +0300

    routes folder / cart.js file: added router.get (get all Carts)

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const Cart = require("../models/Cart");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
 router.post("/", verifyToken, async (req, res) => {
   const newCart = new Cart(req.body);
@@ -40,6 +44,15 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   try {
     const cart = await Cart.findOne({ userId: req.params.userId });
     res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(carts);
   } catch (err) {
     res.status(500).json(err);
   }
