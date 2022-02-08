e4a1674948b6c31928821a8c23aeb57f54edce25
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-08 07:06:03 +0300

    routes folder / cart.js file: added router.get (get user's Cart)

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -36,4 +36,13 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
