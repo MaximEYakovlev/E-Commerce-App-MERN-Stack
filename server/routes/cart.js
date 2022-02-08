c1c8516383db50aa3604e96088df8e8c9b8b821b
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-08 06:55:58 +0300

    routes folder / cart.js file: added router.put (update Cart)

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,12 +1,27 @@
 const router = require("express").Router();
 const Cart = require("../models/Cart");
-const { verifyToken } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
 
 router.post("/", verifyToken, async (req, res) => {
   const newCart = new Cart(req.body);
   try {
     const savedCart = await newCart.save();
     res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
   } catch (err) {
     res.status(500).json(err);
   }
