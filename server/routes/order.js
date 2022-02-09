36f9b71b1d068f887e6c9c8c9b15125b3921d7d1
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-09 21:24:17 +0300

    routes folder / order.js file: added router.post (new Order)

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,3 +1,15 @@
 const router = require("express").Router();
+const Order = require("../models/Order");
+const { verifyToken } = require("./verifyToken");
+
+router.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
