043b1d42a2038239813297b6ff05024e4ed025f3
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-09 22:02:27 +0300

    routes folder / order.js file: added router.delete (delete Order)

--- server/routes/order.js
+++ server/routes/order.js
@@ -27,4 +27,13 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
