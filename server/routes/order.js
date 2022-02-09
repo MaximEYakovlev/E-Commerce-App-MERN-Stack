8fa21ec4ca5e7db70b5862220265cd29b3294103
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-09 21:59:24 +0300

    routes folder / order.js file: added router.put (update Order)

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,12 +1,27 @@
 const router = require("express").Router();
 const Order = require("../models/Order");
-const { verifyToken } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAdmin } = require("./verifyToken");
 
 router.post("/", verifyToken, async (req, res) => {
   const newOrder = new Order(req.body);
   try {
     const savedOrder = await newOrder.save();
     res.status(200).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
   } catch (err) {
     res.status(500).json(err);
   }
