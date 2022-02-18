4e1d41305bb880a9c08c1437c0f30dcaf4d84b22
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-18 23:36:46 +0300

    installed React Router

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,14 @@ import { Login } from "./pages/Login";
 import { Product } from "./pages/Product";
 import { ProductList } from "./pages/ProductList";
 import { Signup } from "./pages/Signup";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Cart />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
