fe77bb63216a12d6fd3909e5691ba943b28c36d9
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-18 22:49:13 +0300

    installed cors

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -16,12 +19,15 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoute);
-app.use("/users", userRoute);
-app.use("/products", productRoute);
-app.use("/cart", cartRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
