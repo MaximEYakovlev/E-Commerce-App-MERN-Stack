975ed255c9cac615ea5348600eaaa17f9fd6dd69
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-19 21:03:17 +0300

    client / App.jsx: created routes

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,11 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/products/:category" element={<ProductList />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
