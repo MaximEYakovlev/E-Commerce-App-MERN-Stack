418881ad4120888f863f4b7cc7bed2ace4736e94
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-19 22:06:18 +0300

    client / CategoryItem.jsx: added Link

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   flex: 1;
@@ -40,11 +41,13 @@ const Button = styled.button`
 export const CategoryItem = ({ item }) => {
   return (
     <Container>
-      <Image src={item.img} />
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>SHOP NOW</Button>
-      </Info>
+      <Link to={`/products/${item.cat}`}>
+        <Image src={item.img} />
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
+      </Link>
     </Container>
   );
 };
