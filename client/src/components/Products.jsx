d4c79f6358ae5feb95c21b7aa24a645cbf8d901c
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-21 12:55:05 +0300

    client / Products.jsx: described the sorting conditions

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -40,6 +40,22 @@ export const Products = ({ cat, filters, sort }) => {
       );
   }, [cat, filters, products]);
 
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
       {filteredProducts.map((item) => (
