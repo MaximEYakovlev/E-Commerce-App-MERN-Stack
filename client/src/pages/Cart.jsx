c24e13151f5e7d2796ae05acd9756c08c60cc7d7
Maxim Yakovlev <it.trainee@yandex.ru>
2022-02-27 23:37:59 +0300

    Cart.jsx: added StripeCheckout to collect online payments

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -7,6 +7,7 @@ import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 import { mobile } from "../responsive";
 import StripeCheckout from "react-stripe-checkout";
+import { useState } from "react";
 
 const KEY = process.env.REACT_APP_STRIPE;
 
@@ -133,6 +134,11 @@ const Button = styled.button`
 
 export const Cart = () => {
   const cart = useSelector((state) => state.cart);
+  const [stripeToken, setStripeToken] = useState(null);
+
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
 
   return (
     <Container>
@@ -199,7 +205,18 @@ export const Cart = () => {
               <SummaryItemText>Total</SummaryItemText>
               <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
             </SummaryItem>
-            <Button>CHECKOUT NOW</Button>
+            <StripeCheckout
+              name="Maxim Shop"
+              image="https://avatars.githubusercontent.com/u/1486366?v=4"
+              billingAddress
+              shippingAddress
+              description={`Your total is $${cart.total}`}
+              amount={cart.total * 100}
+              token={onToken}
+              stripeKey={KEY}
+            >
+              <Button>CHECKOUT NOW</Button>
+            </StripeCheckout>
           </Summary>
         </Bottom>
       </Wrapper>
