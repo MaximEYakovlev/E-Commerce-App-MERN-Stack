b46100a44dfe76491700e32e646899421ddf2bff
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-01 22:41:03 +0300

    Success.jsx: created createOrder function

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,7 +1,47 @@
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router";
+import { userRequest } from "../requestMethods";
 
 export const Success = () => {
   const location = useLocation();
+  const data = location.state.stripeData;
+  const cart = location.state.cart;
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const [orderId, setOrderId] = useState(null);
 
-  return <div>successful</div>;
+  useEffect(() => {
+    const createOrder = async () => {
+      try {
+        const res = await userRequest.post("/orders", {
+          userId: currentUser._id,
+          products: cart.products.map((item) => ({
+            productId: item._id,
+            quantity: item._quantity,
+          })),
+          amount: cart.total,
+          address: data.billing_details.address,
+        });
+        setOrderId(res.data._id);
+      } catch {}
+    };
+    data && createOrder();
+  }, [cart, data, currentUser]);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {orderId
+        ? `Order has been created successfully. Your order number is ${orderId}`
+        : `Successful. Your order is being prepared...`}
+      <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
+    </div>
+  );
 };
