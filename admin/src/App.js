628b6332d30eaa77bd9d1ed2509b657a453c08bc
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-07 21:57:36 +0300

    created Home.jsx

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,5 +1,6 @@
 import { Sidebar } from "./components/sidebar/Sidebar";
 import { Topbar } from "./components/topbar/Topbar";
+import { Home } from "./pages/home/Home";
 import "./app.css";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="others">other pages</div>
+        <Home />
       </div>
     </div>
   );
---
+++ admin/src/pages/home/Home.jsx
@@ -0,0 +1,5 @@
+import "./home.css";
+
+export const Home = () => {
+  return <div className="home">home</div>;
+};
