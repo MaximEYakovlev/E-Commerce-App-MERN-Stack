1cfbe766bd116988b61727188a232836cb391eaf
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-08 14:05:06 +0300

    created FeaturedInfo.jsx

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,5 +1,10 @@
 import "./home.css";
+import { FeaturedInfo } from "../../components/featuredInfo/FeaturedInfo";
 
 export const Home = () => {
-  return <div className="home">home</div>;
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
 };
