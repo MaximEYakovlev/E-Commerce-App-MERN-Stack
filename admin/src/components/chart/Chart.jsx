98df39df4507d3ec3e661ae23fd1361d25c9bfad
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-08 14:43:07 +0300

    created Chart.jsx, chart.css

---
+++ admin/src/components/chart/Chart.jsx
@@ -0,0 +1,9 @@
+import "./chart.css";
+
+export const Chart = () => {
+  return (
+    <div className="chart">
+      <h3 className="chartTitle">Sales Analytics</h3>
+    </div>
+  );
+};
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./home.css";
 import { FeaturedInfo } from "../../components/featuredInfo/FeaturedInfo";
+import { Chart } from "../../components/chart/Chart";
 
 export const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart />
     </div>
   );
 };
