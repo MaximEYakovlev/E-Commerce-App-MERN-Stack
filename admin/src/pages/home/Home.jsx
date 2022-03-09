f1c5725963d774956e912846901b196af67b42b2
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-09 06:03:57 +0300

    Chart.jsx: built a chart using Recharts

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
 import "./home.css";
 import { FeaturedInfo } from "../../components/featuredInfo/FeaturedInfo";
 import { Chart } from "../../components/chart/Chart";
+import { userData } from "../../dummyData";
 
 export const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
