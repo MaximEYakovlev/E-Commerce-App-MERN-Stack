dd513b009e70b5642283e45df2d1e7fff3b6ba39
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-14 23:55:29 +0300

    admin: completed intermediate work on the User.jsx component

--- admin/src/pages/user/User.jsx
+++ admin/src/pages/user/User.jsx
@@ -1,5 +1,56 @@
 import "./user.css";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LocationSearchingIcon from "@mui/icons-material/LocationSearching";
 
 export const User = () => {
-  return <div className="user">user</div>;
+  return (
+    <div className="user">
+      <div className="userTitleContainer">
+        <h1 className="userTitle">Edit User</h1>
+        <button className="userAddButton">Create</button>
+      </div>
+      <div className="userContainer">
+        <div className="userShow">
+          <div className="userShowTop">
+            <img
+              src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="userShowImg"
+            />
+            <div className="userShowTopTitle">
+              <span className="userShowUsername">Anna Becker</span>
+              <span className="userShowUserTitle">Software Engineer</span>
+            </div>
+          </div>
+          <div className="userShowBottom">
+            <span className="userShowTitle">Account Details</span>
+            <div className="userShowInfo">
+              <PermIdentityIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">annabeck99</span>
+            </div>
+            <div className="userShowInfo">
+              <CalendarTodayIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">10.12.1999</span>
+            </div>
+            <div className="userShowInfo">
+              <PhoneAndroidIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">+1 123 456 67</span>
+            </div>
+            <div className="userShowInfo">
+              <MailOutlineIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">annabeck99@gmail.com</span>
+            </div>
+            <div className="userShowInfo">
+              <LocationSearchingIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">New York | USA</span>
+            </div>
+          </div>
+        </div>
+        <div className="userUpdate"></div>
+      </div>
+    </div>
+  );
 };
