c377099962bb5c1e992031954cd0bc3ddd8138b7
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-20 17:54:56 +0300

    admin / Product.jsx: completed work on the component

--- admin/src/dummyData.js
+++ admin/src/dummyData.js
@@ -13,6 +13,21 @@ export const userData = [
   { name: "Dec", "Active User": 3000 },
 ];
 
+export const productData = [
+  {
+    name: "Jan",
+    Sales: 4000,
+  },
+  {
+    name: "Feb",
+    Sales: 3000,
+  },
+  {
+    name: "Mar",
+    Sales: 5000,
+  },
+];
+
 export const userRows = [
   {
     id: 1,
