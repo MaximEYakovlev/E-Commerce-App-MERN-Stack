ebdb340d281cf12f116b55754aaffb2be99d6a45
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-22 21:54:09 +0300

    admin: created Login.jsx

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick}>Login</button>
+    </div>
+  );
+};
