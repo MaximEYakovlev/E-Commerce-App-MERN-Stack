19fc42d35c8520bef9cc62af3e4fb97c6abef27f
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-23 21:37:25 +0300

    admin / Login.jsx: added styles

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -13,18 +13,30 @@ export const Login = () => {
   };
 
   return (
-    <div>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <input
+        style={{ padding: 10, marginBottom: 20 }}
         type="text"
         placeholder="username"
         onChange={(e) => setUsername(e.target.value)}
       />
       <input
+        style={{ padding: 10, marginBottom: 20 }}
         type="password"
         placeholder="password"
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleClick}>Login</button>
+      <button onClick={handleClick} style={{ padding: 10, width: 100 }}>
+        Login
+      </button>
     </div>
   );
 };
