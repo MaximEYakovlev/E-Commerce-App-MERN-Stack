a21d4dc2468f2a134659094242992027de4ed18d
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-26 09:18:22 +0300

    admin: made changes to the Home.jsx file, added a userStats request

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,16 +1,52 @@
 import "./home.css";
 import { FeaturedInfo } from "../../components/featuredInfo/FeaturedInfo";
 import { Chart } from "../../components/chart/Chart";
-import { userData } from "../../dummyData";
 import { WidgetSm } from "../../components/widgetSm/WidgetSm";
 import { WidgetLg } from "../../components/widgetLg/WidgetLg";
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export const Home = () => {
+  const [userStats, setUserStats] = useState([])
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get('/users/stats')
+        res.data.map(item =>
+          setUserStats(prev => [
+            ...prev,
+            { name: MONTHS[item._id - 1], 'Active User': item.total }
+          ])
+        )
+      } catch { }
+    }
+    getStats()
+  }, [MONTHS])
+
   return (
     <div className="home">
       <FeaturedInfo />
       <Chart
-        data={userData}
+        data={userStats}
         title="User Analytics"
         grid
         dataKey="Active User"
