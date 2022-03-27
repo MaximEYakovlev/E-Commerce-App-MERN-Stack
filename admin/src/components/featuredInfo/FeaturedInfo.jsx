f95c13ce624a7b77a55b5effa7b41b3b73230f18
Maxim Yakovlev <maxim.yakovlev@nasc-dev.com>
2022-03-27 19:56:18 +0300

    admin: made changes to the FeaturedInfo.jsx file, added an income request

--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,16 +1,37 @@
 import "./featuredInfo.css";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export const FeaturedInfo = () => {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch {}
+    };
+    getIncome();
+  }, []);
+
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">Revenue</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoney">${income[1]?.total}</span>
           <span className="featuredMoneyRate">
-            -11.4 <ArrowDownwardIcon className="featuredIcon negative" />
+            %{Math.floor(perc)}
+            {perc < 0 ? (
+              <ArrowDownwardIcon className="featuredIcon negative" />
+            ) : (
+              <ArrowUpwardIcon className="featuredIcon" />
+            )}
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
