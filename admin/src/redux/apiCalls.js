fc0b8d666600e1d19eaa0332b55070939c363497
Maxim Yakovlev <it.trainee@yandex.ru>
2022-03-28 20:14:36 +0300

    admin: created the deleteProduct function in the apiCalls.js file; added productReducer to the store.js file; made changes to the ProductList.jsx file

--- admin/src/redux/apiCalls.js
+++ admin/src/redux/apiCalls.js
@@ -1,5 +1,13 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
-import { publicRequest } from "../requestMethods";
+import { publicRequest, userRequest } from "../requestMethods";
+import {
+  getProductFailure,
+  getProductStart,
+  getProductSuccess,
+  deleteProductFailure,
+  deleteProductStart,
+  deleteProductSuccess,
+} from "./productRedux";
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
@@ -18,5 +26,15 @@ export const getProducts = async (dispatch) => {
     dispatch(getProductSuccess(res.data));
   } catch (err) {
     dispatch(getProductFailure());
+  }
+};
+
+export const deleteProduct = async (id, dispatch) => {
+  dispatch(deleteProductStart());
+  try {
+    // const res = await userRequest.delete(`/products/${id}`);
+    dispatch(deleteProductSuccess(id));
+  } catch (err) {
+    dispatch(deleteProductFailure());
   }
 };
