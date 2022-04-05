e594b982ef0718f3457499499626920600cdf992
Maxim Yakovlev <it.trainee@yandex.ru>
2022-04-05 23:24:48 +0300

    admin: installed Firebase, created firebase.js file, added functionality for creating a new product to the NewProduct.jsx file

--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,10 +1,20 @@
 import "./newProduct.css";
 import { useState } from "react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import app from "../../firebase";
+import { addProduct } from "../../redux/apiCalls";
+import { useDispatch } from "react-redux";
 
 export const NewProduct = () => {
   const [inputs, setInputs] = useState({});
   const [file, setFile] = useState(null);
   const [cat, setCat] = useState([]);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setInputs((prev) => {
@@ -14,6 +24,39 @@ export const NewProduct = () => {
 
   const handleCat = (e) => {
     setCat(e.target.value.split(","));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+        }
+      },
+      (error) => {},
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const product = { ...inputs, img: downloadURL, categories: cat };
+          addProduct(product, dispatch);
+        });
+      }
+    );
   };
 
   return (
@@ -66,7 +109,9 @@ export const NewProduct = () => {
             <option value="false">No</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <button onClick={handleClick} className="addProductButton">
+          Create
+        </button>
       </form>
     </div>
   );
